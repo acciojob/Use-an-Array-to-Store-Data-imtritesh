e4cf03f6a32d510fc3962de87a140549aba6c330
imtritesh <103854328+imtritesh@users.noreply.github.com>
2023-05-11 14:17:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
-
+let div = document.querySelector(".sum");
 let yourArray = ["hello world", 42, true, null, "world", 52, false];
+for (let index = 0; index < yourArray.length; index++) {
+	div.textContent += yourArray[index] + " ";
+}
 console.log(yourArray);
