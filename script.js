c2e96b5ea0379a71bca66c7b7f61e1558937d3a7
imtritesh <103854328+imtritesh@users.noreply.github.com>
2023-05-11 14:29:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-let li = document.querySelector(".sum");
+let body = document.body;
 let yourArray = ["hello world", 42, true, null, "world", 52, false];
 for (let index = 0; index < yourArray.length; index++) {
-	li.textContent += yourArray[index] + " ";
+	let li = document.createElement("li");
+	li.textContent = yourArray[index];
+	body.append(li);
 }
-console.log(yourArray);
