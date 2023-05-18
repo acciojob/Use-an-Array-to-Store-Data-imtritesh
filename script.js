1957d5454ac4ec6f06226c5048e5664838e446ed
imtritesh <103854328+imtritesh@users.noreply.github.com>
2023-05-18 17:22:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,3 @@
-let body = document.body;
-let yourArray = ["hello world", 42, true, null, "world", 52, false];
-for (let index = 0; index < yourArray.length; index++) {
-	let li = document.createElement("li");
-	li.textContent = yourArray[index];
-	body.append(li);
-}
 
-module.exports = app;
+let yourArray = ["hello world", 42, true, null, "world", 52, false];
+
